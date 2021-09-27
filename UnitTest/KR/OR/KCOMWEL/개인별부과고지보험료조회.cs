3e0ac140e626bf6d184d3f8e9e566aed3d3ad310
이주영 <juyoung.lee@tilkoblet.com>
2021-09-27 18:47:50 +0900

    [고용산재토탈서비스] 개인별부과고지보험료조회 근로자고용정보현황조회 민원접수현황선택출력 민원접수현황조회 보수총액신고내역조회 부과고지보험료조회 사대보험취득신고 사무수임사업장내역조회 사업장총괄카드조회 일용근로자고용정보현황조회 일자리지원금조회 enum 텍스트파일 추가

--- UnitTest/KR/OR/KCOMWEL/개인별부과고지보험료조회.cs
+++ UnitTest/KR/OR/KCOMWEL/개인별부과고지보험료조회.cs
@@ -6,7 +6,7 @@ using System.IO;
 namespace UnitTest.KR.OR.KCOMWEL
 {
 	[TestClass]
-	public class 사업장총괄카드조회
+	public class 개인별부과고지보험료조회
 	{
 		[TestMethod]
 		public void TILKO_API()
@@ -16,16 +16,18 @@ namespace UnitTest.KR.OR.KCOMWEL
 				Tilko.API.REST _rest		= new Tilko.API.REST(Constant.ApiKey);
 				_rest.Init();
 
-				// 고용산재토탈서비스의 사업장 총괄카드 조회 endPoint 설정
-				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/kcomwel/retrievesaeopjangchonggwalcardjh");
+				// 고용산재토탈서비스의 개인별 부과고지 보험료 조회 endPoint 설정
+				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/kcomwel/selectgaeinbyeolbhrnaeyeok");
 
 				// Body 추가
 				_rest.AddBody("CertFile"      , File.ReadAllBytes(string.Format(@"{0}\signCert.der", Constant.CertPath)), true);
 				_rest.AddBody("KeyFile"       , File.ReadAllBytes(string.Format(@"{0}\signPri.key", Constant.CertPath)), true);
 				_rest.AddBody("CertPassword"  , Constant.CertPassword, true);
 				_rest.AddBody("BusinessNumber", "[조회를 원하는 사업자등록번호]", true);
-				_rest.AddBody("UserGroupFlag" , "0");			// 0: 사업장, 1: 사무대행
-				_rest.AddBody("IndividualFlag", "1");			// 0: 개인, 1: 법인
+				_rest.AddBody("UserGroupFlag" , "0");					// 0: 사업장, 1: 사무대행
+				_rest.AddBody("IndividualFlag", "1");					// 0: 개인, 1: 법인
+				_rest.AddBody("BoheomYear"    , "2021");
+				_rest.AddBody("Geunroja_RgNo" , "[조회를 원하는 근로자주민등록번호 13자리]", true);
 				_rest.AddBody("GwanriNo"      , "");
 
 				// API 호출  
--- UnitTest/KR/OR/KCOMWEL/일용근로자고용정보현황조회.cs
+++ UnitTest/KR/OR/KCOMWEL/일용근로자고용정보현황조회.cs
@@ -6,7 +6,7 @@ using System.IO;
 namespace UnitTest.KR.OR.KCOMWEL
 {
 	[TestClass]
-	public class 사업장총괄카드조회
+	public class 일용근로자고용정보현황조회
 	{
 		[TestMethod]
 		public void TILKO_API()
@@ -16,17 +16,20 @@ namespace UnitTest.KR.OR.KCOMWEL
 				Tilko.API.REST _rest		= new Tilko.API.REST(Constant.ApiKey);
 				_rest.Init();
 
-				// 고용산재토탈서비스의 사업장 총괄카드 조회 endPoint 설정
-				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/kcomwel/retrievesaeopjangchonggwalcardjh");
+				// 고용산재토탈서비스의 일용근로자 고용정보 현황 조회 endPoint 설정
+				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/kcomwel/selectilyonggeunrojainfojohoe");
 
 				// Body 추가
 				_rest.AddBody("CertFile"      , File.ReadAllBytes(string.Format(@"{0}\signCert.der", Constant.CertPath)), true);
 				_rest.AddBody("KeyFile"       , File.ReadAllBytes(string.Format(@"{0}\signPri.key", Constant.CertPath)), true);
 				_rest.AddBody("CertPassword"  , Constant.CertPassword, true);
 				_rest.AddBody("BusinessNumber", "[조회를 원하는 사업자등록번호]", true);
-				_rest.AddBody("UserGroupFlag" , "0");			// 0: 사업장, 1: 사무대행
-				_rest.AddBody("IndividualFlag", "1");			// 0: 개인, 1: 법인
-				_rest.AddBody("GwanriNo"      , "");
+				_rest.AddBody("UserGroupFlag" , "1");					// 0: 사업장, 1: 사무대행
+				_rest.AddBody("IndividualFlag", "1");					// 0: 개인, 1: 법인
+				_rest.AddBody("BoheomFg"      , "2");					// 0: 산재, 1: 고용, 2: 전체
+				_rest.AddBody("GwanriNo"      , "[조회를 원하는 관리번호]");
+				_rest.AddBody("DaesangYYFrom" , "2021");
+				_rest.AddBody("DaesangYYTo"   , "2021");
 
 				// API 호출  
 				string _result				= _rest.Call();
