50c62c82537a219d8d2194412a079bb16ab1bbce
Tilkoblet <help@tilko.net>
2021-09-28 11:14:36 +0900

    건강보험공단의 병원/약국 이용이력 샘플 추가

---
+++ UnitTest/KR/OR/NHIS/병원약국이용이력.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTest.KR.OR.NHIS
+{
+	[TestClass]
+	public class 병원약국이용이력
+	{
+		[TestMethod]
+		public void TILKO_API()
+		{
+			try
+			{
+				Tilko.API.REST _rest		= new Tilko.API.REST(Constant.ApiKey);
+				_rest.Init();
+
+				// 건강보험공단의 병원/약국 이용이력 endPoint 설정
+				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/nhis/retrievecaredesclist");
+
+				// Body 추가
+				_rest.AddBody("CertFile", File.ReadAllBytes(string.Format(@"{0}\signCert.der", Constant.CertPath)), true);
+				_rest.AddBody("KeyFile", File.ReadAllBytes(string.Format(@"{0}\signPri.key", Constant.CertPath)), true);
+				_rest.AddBody("CertPassword", Constant.CertPassword, true);
+				_rest.AddBody("IdentityNumber", "770528", true);
+				_rest.AddBody("StartDate", "20210101");
+				_rest.AddBody("EndDate", "20210701");
+
+				// API 호출
+				string _result				= _rest.Call();
+				if (_rest.HttpStatusCode != System.Net.HttpStatusCode.OK)
+				{
+					throw new Exception(_rest.Message);
+				}
+			}
+			catch (Exception err)
+			{
+				Debug.WriteLine(err.Message);
+			}
+		}
+	}
+}
