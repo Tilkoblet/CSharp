e8eda2aa9869eb0a97480e9142e77918a0256f61
Seongwon Seo <seongwon.seo@tilkoblet.com>
2021-12-24 10:08:59 +0900

    .

---
+++ UnitTest/KR/GOV/간편인증요청.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTest.KR.GOV
+{
+	[TestClass]
+	public class 간편인증요청
+	{
+		[TestMethod]
+		public void TILKO_API()
+		{
+			// API 상세설명 URL
+			// https://tilko.net/Help/Api/POST-api-apiVersion-GovSimpleAuth-SimpleAuthRequest
+
+			try
+			{
+				Tilko.API.REST _rest = new Tilko.API.REST(Constant.ApiKey);
+				_rest.Init();
+
+				// 정부24의 간편인증 요청 endPoint 설정
+				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/GovSimpleAuth/SimpleAuthRequest");
+
+				// Body 추가
+				_rest.AddBody("PrivateAuthType", "4", false);	// 인증종류 0: 카카오톡 / 1: 페이코 / 2: 국민은행모바일 / 3: 삼성패스 / 4: Pass
+				_rest.AddBody("UserName", "홍길동", true);                  // [암호화] 이용자명
+				_rest.AddBody("BirthDate", "20210101", true);               // [암호화] 생년월일(yyyyMMdd)
+				_rest.AddBody("UserCellphoneNumber", "01012345678", true);  // [암호화] 휴대폰번호
+
+				// API 호출
+				string _result = _rest.Call();
+				if (_rest.HttpStatusCode != System.Net.HttpStatusCode.OK)
+				{
+					throw new Exception(_rest.Message);
+				}
+			}
+			catch (Exception err)
+			{
+				Debug.WriteLine(err.Message);
+			}
+		}
+	}
+}
---
+++ UnitTest/KR/GOV/주민등록증진위여부-간편인증용.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTest.KR.GOV
+{
+	[TestClass]
+	public class 주민등록증진위여부간편인증용
+	{
+		[TestMethod]
+		public void TILKO_API()
+		{
+			try
+			{
+				// API 상세설명 URL
+				// https://tilko.net/Help/Api/POST-api-apiVersion-GovSimpleAuth-AA090UserJuminCheckResApp
+
+				Tilko.API.REST _rest = new Tilko.API.REST(Constant.ApiKey);
+				_rest.Init();
+
+				// 정부24의 주민등록증진위여부 endPoint 설정
+				_rest.SetEndPointUrl(Constant.ApiHost + "/api/v1.0/GovSimpleAuth/AA090UserJuminCheckResApp");
+
+				// Body 추가
+				_rest.AddBody("CxId", "", false);				// 간편인증 요청 후 받은 CxId 값
+				_rest.AddBody("PrivateAuthType", "", false);	// 간편인증 요청 후 받은 PrivateAuthType 값
+				_rest.AddBody("ReqTxId", "", false);			// 간편인증 요청 후 받은 ReqTxId 값
+				_rest.AddBody("Token", "", false);				// 간편인증 요청 후 받은 Token 값
+				_rest.AddBody("TxId", "", false);				// 간편인증 요청 후 받은 TxId 값
+				_rest.AddBody("UserName", "", true);			// [암호화] 간편인증 요청 후 받은 UserName 값
+				_rest.AddBody("BirthDate", "", true);			// [암호화] 간편인증 요청 후 받은 BirthDate 값
+				_rest.AddBody("UserCellphoneNumber", "", true);	// [암호화] 간편인증 요청 후 받은 UserCellphoneNumber 값
+				_rest.AddBody("PersonName", "", true);			// [암호화] 조회 대상자의 성명
+				_rest.AddBody("IdentityNumber", "", true);		// [암호화] 조회 대상자의 주민등록번호(9001011234567)
+				_rest.AddBody("PublishDate", "", false);		// 주민등록증 발행일(yyyyMMdd)
+
+				// API 호출
+				string _result = _rest.Call();
+				if (_rest.HttpStatusCode != System.Net.HttpStatusCode.OK)
+				{
+					throw new Exception(_rest.Message);
+				}
+			}
+			catch (Exception err)
+			{
+				Debug.WriteLine(err.Message);
+			}
+		}
+	}
+}
